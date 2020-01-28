a34bec9eaa7d7e8c7474afa24196e5c8df68029c
Pursuit <pursuit@Pursuits-MacBook-Pro-3.local>
2020-01-28 15:30:21 -0500

    Loaded UI objects and constraints to VC

--- Kwiz/ViewControllers/MultipleChoiceVC.swift
+++ Kwiz/ViewControllers/MultipleChoiceVC.swift
@@ -10,13 +10,113 @@ import UIKit
 
 class MultipleChoiceVC: UIViewController {
     
+    lazy var questionTextField: UITextField = {
+        let tf = UITextField()
+        tf.font = UIFont(name: "Times New Roman", size: 14)
+        tf.backgroundColor = .white
+        return tf
+        
+    }()
     
+    lazy var answerChoiceATextField: UITextField = {
+        let tf = UITextField()
+        tf.font = UIFont(name: "Times New Roman", size: 14)
+        tf.backgroundColor = .white
+        return tf
+        
+    }()
+    
+    lazy var answerChoiceBTextField: UITextField = {
+        let tf = UITextField()
+        tf.font = UIFont(name: "Times New Roman", size: 14)
+        tf.backgroundColor = .white
+        return tf
+        
+    }()
+    
+    lazy var answerChoiceCTextField: UITextField = {
+        let tf = UITextField()
+        tf.font = UIFont(name: "Times New Roman", size: 14)
+        tf.backgroundColor = .white
+        return tf
+        
+    }()
+    
+    lazy var answerChoiceDTextField: UITextField = {
+        let tf = UITextField()
+        tf.font = UIFont(name: "Times New Roman", size: 14)
+        tf.backgroundColor = .white
+        return tf
+        
+    }()
+    
+    lazy var userLivesImage: UIImageView = {
+        let image = UIImageView()
+        
+        return image
+    }()
 
+    
+    lazy var skipButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    lazy var backButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addViews()
+        addConstraints()
         view.backgroundColor = .blue
     }
 
+    private func addViews(){
+        view.addSubview(questionTextField)
+        view.addSubview(answerChoiceATextField)
+        view.addSubview(answerChoiceBTextField)
+        view.addSubview(answerChoiceCTextField)
+        view.addSubview(answerChoiceDTextField)
+        view.addSubview(skipButton)
+        view.addSubview(backButton)
+        
+    }
+    
+    private func addConstraints(){
+        setUpQuestionTF()
+        setUpAnswersStackView()
+        
+    }
+    
+    private func setUpQuestionTF(){
+        questionTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            questionTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            questionTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            questionTextField.bottomAnchor.constraint(equalTo: answerChoiceATextField.topAnchor, constant: 50),
+        ])
+        
+    }
+    
+    private func setUpAnswersStackView(){
+        let stackView = UIStackView(arrangedSubviews: [answerChoiceATextField, answerChoiceBTextField, answerChoiceCTextField, answerChoiceDTextField])
+        stackView.axis = .vertical
+        stackView.spacing = 15
+        stackView.distribution = .fillEqually
+        self.view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            stackView.heightAnchor.constraint(equalToConstant: 130)])
+    }
+    
 
 }
 
