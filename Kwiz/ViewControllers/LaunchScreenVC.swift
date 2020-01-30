6c126f64614e26216aff1777b7e20f4c0d7cb69b
eeeric3995@yahoo.com <ihxx2s7tbu0>
2020-01-30 16:50:43 -0500

    made launch screen

--- Kwiz/ViewControllers/LaunchScreenVC.swift
+++ Kwiz/ViewControllers/LaunchScreenVC.swift
@@ -9,11 +9,50 @@
 import UIKit
 
 class LaunchScreenVC: UIViewController {
-
+    
+    lazy var image: UIImageView = {
+        var image = UIImageView()
+        image.contentMode = UIView.ContentMode.scaleAspectFit
+        image.autoresizingMask = [UIView.AutoresizingMask.flexibleBottomMargin, UIView.AutoresizingMask.flexibleHeight, UIView.AutoresizingMask.flexibleRightMargin, UIView.AutoresizingMask.flexibleLeftMargin, UIView.AutoresizingMask.flexibleTopMargin, UIView.AutoresizingMask.flexibleWidth]
+        image.image = UIImage(named: "logo")
+        return image
+    }()
+    
+    lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Start", for: .normal)
+        button.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        button.layer.cornerRadius = 5
+        //button.addTarget(self, action: #selector(changeStatus), for: .touchUpInside)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = #colorLiteral(red: 0.7802590728, green: 0.932610333, blue: 0.9999867082, alpha: 1)
+        imageConstraint()
+        
     }
-
+    
+    func imageConstraint(){
+        button.translatesAutoresizingMaskIntoConstraints = false
+        image.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(image)
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            image.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            image.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            image.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            image.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            button.heightAnchor.constraint(equalToConstant: 50),
+            button.widthAnchor.constraint(equalToConstant: 150),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: view.bottomAnchor, constant: -200)
+        ])
+    }
+    
 }
 
