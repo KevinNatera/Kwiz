c1fe33efca4043726db92bdf67eacb74a698b165
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-01-30 15:32:58 -0500

    adds buttons to vc

--- Kwiz/ViewControllers/MainVC.swift
+++ Kwiz/ViewControllers/MainVC.swift
@@ -9,22 +9,54 @@
 import UIKit
 
 class MainVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    //MARK: - UIObjects
+    var startButton = UIButton()
+    var rankingButton = UIButton()
+    var settingButton = UIButton()
+    
+    //MARK: - Setup
+    private func setupStackViewWithButtons() {
+        let stackView = UIStackView(arrangedSubviews: [startButton,rankingButton,settingButton])
+        stackView.axis = .vertical
+        stackView.spacing = view.frame.size.height / 9
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.size.height / 3)])
+    }
+    private func setupButtons() {
+        updateTitleOnButton(button: startButton, title: "START")
+        updateTitleOnButton(button: rankingButton, title: "RANKING")
+        updateTitleOnButton(button: settingButton, title: "SETTINGS")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Functions
+    private func updateTitleOnButton(button: UIButton, title: String) {
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.6798086851, green: 0.9229053351, blue: 0.9803921569, alpha: 1)
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 40)
+        button.contentEdgeInsets = UIEdgeInsets(top: 20, left: 40, bottom: 20, right: 40)
+        
+        button.frame = CGRect(x: 0, y: 0, width: view.frame.size.width * 4 / 5, height: view.frame.size.height / 8)
+        
+        button.layer.cornerRadius = button.frame.size.height / 2 - 15
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
     }
-    */
+
+    //MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.4241105914, green: 0.3439905345, blue: 0.7182968259, alpha: 1)
+        setupButtons()
+        setupStackViewWithButtons()
+
+    }
 
 }
