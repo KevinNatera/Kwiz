d91b52485df408dc2dd17bb820c077fd1f57dc10
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-02-03 12:16:09 -0500

    adds ScaleVC

---
+++ Kwiz/ViewControllers/ScaleVC.swift
@@ -0,0 +1,30 @@
+//
+//  ScaleVC.swift
+//  Kwiz
+//
+//  Created by Angela Garrovillas on 2/3/20.
+//  Copyright © 2020 2Legit2Quit. All rights reserved.
+//
+
+import UIKit
+
+class ScaleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
