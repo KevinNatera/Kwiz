87b7e0ff55bdccc7a88fd495377e0db2a31c26b1
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-02-03 12:27:10 -0500

    adds line

--- Kwiz/ViewControllers/ScaleVC.swift
+++ Kwiz/ViewControllers/ScaleVC.swift
@@ -9,22 +9,36 @@
 import UIKit
 
 class ScaleVC: UIViewController {
+    //MARK: - UIObjects
+    var line: UIView = {
+        let lineView = UIView(frame: CGRect(x: 0, y: 0, width: 360, height: 25))
+        lineView.backgroundColor = .black
+        return lineView
+    }()
+    
+    //MARK: - Constraints
+    private func setConstraints() {
+        constrainLine()
+    }
+    private func constrainLine() {
+        view.addSubview(line)
+        line.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            line.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            line.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            line.widthAnchor.constraint(equalToConstant: line.frame.width),
+            line.heightAnchor.constraint(equalToConstant: line.frame.height)])
+    }
 
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setConstraints()
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
