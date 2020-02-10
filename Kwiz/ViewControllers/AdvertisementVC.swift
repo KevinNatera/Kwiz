0678f0ee7e74954d3c194e515395c3c8a0f15349
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-02-10 16:18:07 -0500

    adds Advertisement vc

---
+++ Kwiz/ViewControllers/AdvertisementVC.swift
@@ -0,0 +1,30 @@
+//
+//  AdvertisementVC.swift
+//  Kwiz
+//
+//  Created by Angela Garrovillas on 2/10/20.
+//  Copyright © 2020 2Legit2Quit. All rights reserved.
+//
+
+import UIKit
+
+class AdvertisementVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
