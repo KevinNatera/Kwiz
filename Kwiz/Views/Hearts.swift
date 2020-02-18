b351fef0b9811044849e553442bc5779495c09dc
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-02-18 11:24:52 -0500

    adds init for HeartsStackView

--- Kwiz/Views/Hearts.swift
+++ Kwiz/Views/Hearts.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 2Legit2Quit. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class HeartsStackView: UIStackView {
+    //MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupStackView()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    //MARK: Setup Functions
+    func setupStackView() {
+        axis = .horizontal
+        spacing = 1
+        distribution = .fillEqually
+    }
+}
