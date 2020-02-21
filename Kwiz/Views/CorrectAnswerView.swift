99b8ff3484eba450c745758c16acebd379ba904a
Pursuit <pursuit@Pursuits-MacBook-Pro-3.local>
2020-02-21 16:21:18 -0500

    Created alerts for answer results

--- Kwiz/Views/CorrectAnswerView.swift
+++ Kwiz/Views/CorrectAnswerView.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2020 2Legit2Quit. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum UserResult {
+    case correct
+    case wrong
+}
+
+class AnswerAlert {
+    static let shared = AnswerAlert()
+    
+    func answerResult(userResult:UserResult, viewController: UIViewController){
+        switch userResult {
+        case .correct:
+            let alert = UIAlertController(title: "Correct!", message: "Congratulations! You gained 5 points!", preferredStyle: .alert)
+            viewController.present(alert, animated: true)
+            
+            
+        case .wrong:
+            let alert = UIAlertController(title: "Wrong", message: "Try again!", preferredStyle: .alert)
+            viewController.present(alert, animated: true)
+            
+        }
+    }
+    
+    
+}
+    
+   
+
+    
+    
+    
+    
+    
+
+
