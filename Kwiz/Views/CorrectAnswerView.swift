4a0dded147a41987bb366c7ae2b949ef41a4251e
Pursuit <pursuit@Pursuits-MacBook-Pro-3.local>
2020-02-24 12:23:36 -0500

    Added alerts to CorrectAnswerView

--- Kwiz/Views/CorrectAnswerView.swift
+++ Kwiz/Views/CorrectAnswerView.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 enum UserResult {
     case correct
     case wrong
@@ -17,14 +18,22 @@ class AnswerAlert {
     static let shared = AnswerAlert()
     
     func answerResult(userResult:UserResult, viewController: UIViewController){
+        
         switch userResult {
         case .correct:
             let alert = UIAlertController(title: "Correct!", message: "Congratulations! You gained 5 points!", preferredStyle: .alert)
             viewController.present(alert, animated: true)
+            let when = DispatchTime.now() + 2
+            DispatchQueue.main.asyncAfter(deadline: when){
+              alert.dismiss(animated: true, completion: nil)
+            }
+            
+            
             
             
         case .wrong:
             let alert = UIAlertController(title: "Wrong", message: "Try again!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
             viewController.present(alert, animated: true)
             
         }
