7f044bf3ada582123439da8b45b3d1d2d1971e05
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-03-02 10:31:32 -0500

    adds func to switch on specialQuestion to get specific VC

--- Kwiz/Models/GetVCFromSpecialQuestion.swift
+++ Kwiz/Models/GetVCFromSpecialQuestion.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2020 2Legit2Quit. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+func getVCFromSpecial(question: SpecialQuestion?) -> UIViewController {
+    switch question {
+    case is Advertisement:
+        return AdvertisementVC()
+    case is Tickle:
+        return TickleQuestionVC()
+    case is Scale:
+        return ScaleVC()
+    case is Lock:
+        return LockVC()
+    case .none:
+        return ResultsVC()
+    default:
+        return ResultsVC()
+    }
+}
